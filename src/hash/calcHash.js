5ae8b5f4e1a2a9f0aea439ce649b1ec55eedea9b
Tonya Mazakova <apmazakova@gmail.com>
2022-12-02 13:57:46 +0300

    Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,13 @@
+import { createHash } from 'node:crypto'
+import { readFile } from 'node:fs';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    readFile('src/hash/files/fileToCalculateHashFor.txt', { encoding: 'utf8' }, (err, content) => {
+        if (err) throw err
+
+        const hex = createHash('sha256').update(content).digest('hex')
+        console.log(hex)
+    })
 };
+
+calculateHash()
