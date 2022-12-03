2d147d78ff4e02b5b8c82e147251e197654ee999
Tonya Mazakova <apmazakova@gmail.com>
2022-12-03 18:04:26 +0300

    Zlib (src/zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,11 @@
+import { createGzip } from 'node:zlib';
+import { createWriteStream, createReadStream } from 'node:fs';
+
 export const compress = async () => {
-    // Write your code here 
+    const readStream = await createReadStream("src/zip/files/fileToCompress.txt")
+    const writeStream = await createWriteStream("src/zip/files/archive.gz")
+
+    readStream.pipe(createGzip()).pipe(writeStream);
 };
+
+compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,11 @@
+import { createUnzip } from 'node:zlib';
+import { createWriteStream, createReadStream } from 'node:fs';
+
 export const decompress = async () => {
-    // Write your code here 
+    const readStream = await createReadStream("src/zip/files/archive.gz")
+    const writeStream = await createWriteStream("src/zip/files/fileToCompress.txt")
+
+    readStream.pipe(createUnzip()).pipe(writeStream);
 };
+
+decompress()
