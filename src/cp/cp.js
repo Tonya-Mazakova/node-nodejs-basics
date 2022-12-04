d7a77db2aa34700ffa01462f8631bb24acc962f3
Tonya Mazakova <apmazakova@gmail.com>
2022-12-04 23:51:41 +0300

    Child Processes (src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import { fork } from 'node:child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+   const child = fork('src/cp/files/script.js', args);
+
+    child.on('error', (error) => {
+        console.error(`error: ${error.message}`);
+    });
+
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
 };
+
+spawnChildProcess(process.argv)
